4d8a3afe81900d9758112e40a7bd1d8f30aa2027
Rahij <rahijj@gmail.com>
2020-10-28 03:04:02 +0500

    Added files

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -51,4 +51,18 @@ workbox.routing.registerRoute(
 			})
 		]
 	})
+)workbox.routing.registerRoute(
+	({ url, event }) => {
+		const match = url.host === "storage.googleapis.com" || url.host === "fonts.googleapis.com"
+		return match
+	},
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: 'images',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 30,
+				purgeOnQuotaError: true
+			})
+		]
+	})
 )
