978430af11e8659e99bd34065f2ff05f1a19f68a
Rahij <rahijj@gmail.com>
2020-10-28 02:26:46 +0500

    Added files

--- build/service-worker.js
+++ build/service-worker.js
@@ -37,3 +37,18 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
+workbox.routing.registerRoute(
+	({ url, event }) => {
+		const match = url.host === "storage.googleapis.com" || url.host === "fonts.googleapis.com"
+		return match
+	},
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: 'images',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxAgeSeconds: 60 * 60 * 24 * 30,
+				purgeOnQuotaError: true
+			})
+		]
+	})
+)
