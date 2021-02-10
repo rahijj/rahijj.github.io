3e9f788d6f14e74d99fdea82cb8d75fb8b408b6a
Rahij Gillani <rahijj@gmail.com>
2021-02-10 08:42:36 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46389887343da54666922ff356f3f60e.js"
+  "/precache-manifest.369b360b9efa4c28bf756ed842517011.js"
 );
 
 self.addEventListener('message', (event) => {
